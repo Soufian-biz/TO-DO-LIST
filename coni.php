4f165615693cfb8041139e01a17ec5c98486a7c5
Soufian Bizguern <156693891+Soufian-biz@users.noreply.github.com>
2024-07-05 15:28:06 +0100

    Add files via upload

---
+++ coni.php
@@ -0,0 +1,17 @@
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=todo', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if (isset($_POST['do']) && !empty($_POST['do'])) {
+        $stmt = $pdo->prepare("INSERT INTO list (do) VALUES (?)");
+        $stmt->execute([$_POST["do"]]);
+      
+    } else {
+        echo "Error: 'do' is not set or is empty";
+    }
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+header("Location:index.php");
+?>
